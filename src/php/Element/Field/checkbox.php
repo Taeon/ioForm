6bf1a1b7f753f7373d5a6e9d634605301206521c
Taeon <github-taeon@filofox.com>
2018-01-31 17:50:25 +0000

    Checkbox value

--- src/php/Element/Field/checkbox.php
+++ src/php/Element/Field/checkbox.php
@@ -7,10 +7,13 @@ class checkbox extends \ioForm\Element\Field{
 	protected $is_singleton = true;
 	protected $attributes = array(
 		'type' => 'checkbox'
-	);	
-	
+	);
+
 	public function __construct( $element_definition ){
 		parent::__construct( $element_definition );
+		if( isset( $element_definition->value ) ){
+			$this->setAttribute( 'value', (string)$element_definition->value );
+		}
 		if( $element_definition->default ){
 			$this->SetValue( true );
 		}
